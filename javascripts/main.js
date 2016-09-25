ca48e1a6df0a98a6dc4f0f1678822572213c8f8e
candidosales <candidosg@gmail.com>
2016-09-25 18:31:38 -0300

    add icons

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,11 @@
-// var allCategories = document.getElementsByClassName("all-categories");
 
-// allCategories[0].addEventListener("mouseover", function(){ 
-//     this.setAttribute("class", "menu-main-item all-categories active");    
-// });
-
-// allCategories[0].addEventListener("mouseout", function(){ 
-//     this.setAttribute("class", "menu-main-item all-categories");    
-// });
 
 
 
 
 $(document).ready(function(){
 
-  jQuery('.all-categories').on('mouseover', function(){
+    $('.all-categories').on('mouseover', function(){
         $(this).addClass('active');
     }).on('mouseout', function(){
         $(this).removeClass('active');
